54e28554c3d34f60388ca535bacf2b008c9f4ce3
k-kashapov <52855633+k-kashapov@users.noreply.github.com>
2021-10-07 22:25:46 +0300

    Add files via upload

--- Processor.cpp
+++ Processor.cpp
@@ -10,7 +10,9 @@ int main(int argc, const char** argv)
 
     read_all_lines (&file, io_config.input_file);
 
-    printf ("\x23include \"Processor.h\"\nint main (int argc, const char** argv)\n{\n    bgn();\n");
+    FILE* out = fopen ("aaa.cpp", "wt");
+
+    fprintf (out, "\x23include \"Processor.h\"\nint main (int argc, const char** argv)\n{\n    bgn();\n");
 
     for (int line = 0; line < file.lines_num; line++)
     {
@@ -18,11 +20,12 @@ int main(int argc, const char** argv)
         float val = 0;
         sscanf (file.strs[line]->text, "%s %f", command, &val);
 
-        if (!strcmp (command, "push")) printf ("    push (%f);\n", val);
+        if (!strcmp (command, "push")) fprintf (out, "    push (%f);\n", val);
          
     }
 
-    printf ("    return 0;\n}"); 
+    fprintf (out, "    return 0;\n}"); 
 
+    fclose (out);
     return 0;
 }
